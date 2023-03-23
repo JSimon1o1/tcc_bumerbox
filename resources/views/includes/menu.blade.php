eaf0623a3672b9708ea7b2ba8a505f2f6b585f9c
Rovian Vieceli <rovianvieceli@gmail.com>
2023-03-23 12:06:55 -0300

    Ajusta para receber o menu

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,30 @@
+<div class="position-sticky menu-lateral">
+    <ul class="nav flex-column bg-verde-limao">
+        <h6 class="menu-opcao d-flex">Cadastros</h6>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Fornecedores</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Categoria</a>
+        </li>
+
+        <h6 class="menu-opcao d-flex">Cadastros</h6>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Relatórios</a>
+        </li>
+
+        <h6 class="menu-opcao d-flex">Configurações</h6>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Minha Conta</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sair</a>
+        </li>
+    </ul>
+</div>
