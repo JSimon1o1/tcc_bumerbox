b187d2e20313f376955882fcfa71598f02379a6c
Rovian Vieceli <rovianvieceli@gmail.com>
2023-04-16 23:59:09 -0300

    Fix > adicionado icones

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,10 +2,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Bumerbox - Controle de Recicláveis">
 <meta name="author"
-      content="Bianca Drum, Israel Comazzetto dos Reis, Júlia Simões Fagundes, Juliana Mignoni, Josué Schiavini, Rafael Pereira Lopes, Rovian Vieceli">
+      content="Bianca Drum, Israel Comazzetto dos Reis, Júlia Simões Fagundes, Juliana Mignoni, Josué Schiavini,
+      Rafael Pereira Lopes, Rovian Vieceli">
 <meta name="keywords" content="HTML, CSS, JavaScript, PHP">
-<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<meta name="theme-color" content="#712cf9">
 <meta name="theme-color" content="#712cf9">
 <link rel="icon" href="{{ asset('favicon.ico') }}">
-<meta name="theme-color" content="#712cf9">
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('css/fontawesome.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
